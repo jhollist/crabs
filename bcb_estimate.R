07113b17e10aff25938ad15606eb283fc6fdfa06
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-08-10 11:54:01 -0400

    getting marshes pulled out

--- bcb_estimate.R
+++ bcb_estimate.R
@@ -10,24 +10,38 @@ library(tidyverse)
 #              destfile = "data/saltmarsh.zip")
 # unzip("data/saltmarsh.zip",exdir = "data")
 saltmarsh12 <- st_read(here("data/saltmarsh12.shp"))
-pass <- st_read(here("data/pass.shp")) %>%
-  mutate(marsh = "pass") %>%
-  select(id = Id, marsh)
+
 biss <- st_read(here("data/bissel.shp")) %>%
   mutate(marsh = "biss") %>%
-  select(id = Id, marsh)
-cogg <- st_read(here("data/coggeshall_w_Iva.shp")) %>% 
-  mutate(marsh = "cogg") %>%
-  select(id = Id, marsh)
-nag <- st_read(here("data/nag_bound.shp")) %>%
-  mutate(marsh = "nag") %>%
-  select(id = Id, marsh)
-
-crab_marsh_bnd <- rbind(pass,biss,cogg,nag) %>%
-  mutate(tot_area = st_area(.)) %>%
+  select(id = Id, marsh) %>%
+  mutate(tot_area = st_area(.) ) %>%
   group_by(marsh) %>%
   summarize(tot_area = sum(tot_area))
-crab_marsh_bnd
+
+biss_idx <- biss %>% 
+  st_intersects(saltmarsh12) %>%
+  unlist
+
+cogg <- st_read(here("data/coggeshall_w_Iva.shp")) %>% 
+  mutate(marsh = "cogg") %>%
+  select(id = Id, marsh) %>%
+  mutate(tot_area = st_area(.) ) %>%
+  group_by(marsh) %>%
+  summarize(tot_area = sum(tot_area))
+
+nag <- st_read(here("data/nag_bound.shp")) %>%
+  mutate(marsh = "nag") %>%
+  select(id = Id, marsh) %>%
+  mutate(tot_area = st_area(.) ) %>%
+  group_by(marsh) %>%
+  summarize(tot_area = sum(tot_area))
+
+pass <- st_read(here("data/pass.shp")) %>%
+  mutate(marsh = "pass") %>%
+  select(id = Id, marsh) %>%
+  mutate(tot_area = st_area(.) ) %>%
+  group_by(marsh) %>%
+  summarize(tot_area = sum(tot_area))
 
 crab_marsh_hab <- crab_marsh_bnd %>%
   st_union(saltmarsh12)
