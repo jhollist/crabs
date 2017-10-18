f87c671501bf143fa9aeebad918aa531af3879bc
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-10-18 14:46:11 -0400

    got some correlations run

--- R/data_prep.R
+++ R/data_prep.R
@@ -9,3 +9,7 @@ library(stringr)
 
 ssht <- here::here("data/jho_correlation_data.xlsx")
 all_sheets <- lapply(excel_sheets(ssht), read_excel, path = ssht)
+names(all_sheets)<-excel_sheets(ssht)
+
+cor(all_sheets$`Burrows VCB`[,-1], all_sheets$`Burrows VCB`$`mean burrows`,
+    use = "pairwise.complete.obs")
