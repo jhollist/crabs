6debe5c5e6bd0c37a6b5caea41adbebe63883790
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-10-19 15:31:48 -0400

    correlation data cleaned up.

---
+++ R/correlation_matrix.R
@@ -0,0 +1,34 @@
+# Packages
+library(tidyverse)
+library(here)
+library(stringr)
+library(hrbrthemes)
+source("R/corr_fig.R")
+
+wi <- unique(read_csv(here("data/watershed_integrity_new_2.csv")))
+
+x <- c("iwi", "ici","wchem","cchem","whabt","chabt","wsed","csed","whyd",
+           "chyd","wtemp","ctemp","wconn","cconn")
+
+watersheds <- unique(wi$watershed)
+
+for(i in watersheds){
+  ws_abb <- abbreviate(paste(i,collapse = ""))
+  
+  if(ws_abb == "chpt"){
+    mtd <- "spearman"
+  } else {
+    mtd <- "pearson"
+  }
+  
+  wi %>%
+    filter(!str_detect(wi$variable, paste(x,collapse = "|"))) %>%
+    filter(!grepl("_old",variable)) %>%
+    filter(watershed == i) %>%
+    pull(variable) %>%
+    unique() %>%
+    corr_fig(data = wi, ws = i, x = x, y = ., 
+             output_csv = paste0("data/", ws_abb, "_cor.csv"),
+             filename = paste0("figures/", ws_abb, "_cor_fig.jpg"), 
+             width = 7,height = 5, units = "in", dpi = 300, method = mtd)
+}
--- R/data_prep.R
+++ R/data_prep.R
@@ -1,7 +1,6 @@
 # Packages
 library(dplyr)
 library(tidyr)
-library(lubridate)
 library(here)
 library(readxl)
 library(readr)
@@ -11,5 +10,32 @@ ssht <- here::here("data/jho_correlation_data.xlsx")
 all_sheets <- lapply(excel_sheets(ssht), read_excel, path = ssht)
 names(all_sheets)<-excel_sheets(ssht)
 
-cor(all_sheets$`Burrows VCB`[,-1], all_sheets$`Burrows VCB`$`mean burrows`,
-    use = "pairwise.complete.obs")
+crab_cor_data <- data.frame()
+for(i in 1:length(all_sheets)){
+  xdf <- all_sheets[[i]] %>%
+    mutate(habitat = stringr::str_split(names(all_sheets)[i]," ")[[1]][2],
+           site_id = row.names(.))
+  if(names(xdf)[1] == "CPUE"){
+    names(xdf)[1] <- paste(stringr::str_split(names(all_sheets)[i]," ")[[1]][1],
+                          "cpue")
+  } else {
+    names(xdf)[1] <- "burrow density"
+  }
+  crab_cor_data <- xdf %>%
+    gather(variable, value, c(-habitat, -site_id)) %>%
+    rbind(.,crab_cor_data)
+}
+
+crab_cor_data <- crab_cor_data %>%
+  mutate(habitat = str_to_lower(habitat),
+         variable = str_to_lower(variable)) %>%
+  mutate(variable = case_when(grepl(" - ", variable) ~
+                                str_replace(variable, " - ", "_"),
+                              grepl(" ", variable) ~ 
+                                str_replace(variable, " ", "_"),
+                              TRUE ~ variable)) %>%
+  unique()
+
+write_csv(crab_cor_data,here("data/crab_corr_data.csv"))
+              
+
