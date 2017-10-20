0b859ab3e26f7601c8ddd00a0c328e27e5c167a0
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-10-20 10:42:17 -0400

    pulled out r, n, and p

--- R/corr_fig_develop.R
+++ R/corr_fig_develop.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(here)
 library(ggplot2)
+library(psych)
 
 crab_corr_data <- read_csv(here("data/crab_corr_data.csv"))
 
@@ -8,8 +9,20 @@ bcb_corr <- crab_corr_data %>%
   spread(variable, value) %>%
   filter(habitat == "bcb") %>%
   select(-habitat,-site_id) %>%
-  cor(use="pairwise.complete.obs") %>%
-  data.frame %>%
+  corr.test(use="pairwise")
+
+bcb_corr_r <- data.frame(bcb_corr$r) %>%
+  mutate(variables = row.names(.)) %>%
+  select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
+bcb_corr_n <- data.frame(bcb_corr$n) %>%
+  mutate(variables = row.names(.)) %>%
+  select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
+bcb_corr_p <- data.frame(bcb_corr$p) %>%
+  mutate(variables = row.names(.)) %>%
+  select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
+  
+  
+  data.frame(.$r) %>%
   mutate(variables = row.names(.)) %>%
   select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
 bcb_corr
