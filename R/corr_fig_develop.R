afcd0555701a2cc1f6a4f78cc0975229b84968b3
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-10-23 15:53:02 -0400

    close to having correlations calc'ed

--- R/corr_fig_develop.R
+++ R/corr_fig_develop.R
@@ -3,7 +3,47 @@ library(here)
 library(ggplot2)
 library(psych)
 
+calc_correlation <- function(xdf, hab){
+  parse_it <- function(df){
+    df %>%
+      mutate(variables = row.names(.)) %>%
+      select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue) %>%
+      filter(variables != "burrow_density",
+             variables != "carcinus_cpue",
+             variables != "sesarma_cpue",
+             variables != "uca_cpue") %>%
+      mutate(habitat = hab)
+  }
+  
+  corr_df <- xdf %>%
+      spread(variable, value) %>%
+      filter(habitat == hab) %>%
+      select(-habitat,-site_id) %>%
+      corr.test(use="pairwise")
+  
+  r <- data.frame(corr_df$r) %>%
+    parse_it() %>%
+    gather(crab_params, pearson_cor,2:5) %>%
+    select(habitat,crab_params, env_params = variables, pearson_cor)
+  n <- data.frame(corr_df$n) %>%
+    parse_it() %>%
+    gather(crab_params, n,2:5) %>%
+    select(habitat,crab_params, env_params = variables, n)
+  p <- data.frame(corr_df$p) %>%
+    parse_it() %>%
+    gather(crab_params, p,2:5) %>%
+    select(habitat,crab_params, env_params = variables, p)
+  
+  left_join(r,n) %>%
+    left_join(p)
+}
+
 crab_corr_data <- read_csv(here("data/crab_corr_data.csv"))
+
+for(i in unique(crab_cor_data$habitat)){
+  calc_correlation(xdf, i)
+}
+
 
 bcb_corr <- crab_corr_data %>%
   spread(variable, value) %>%
@@ -13,7 +53,15 @@ bcb_corr <- crab_corr_data %>%
 
 bcb_corr_r <- data.frame(bcb_corr$r) %>%
   mutate(variables = row.names(.)) %>%
-  select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
+  select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue) %>%
+  filter(variables != "burrow_density",
+         variables != "carcinus_cpue",
+         variables != "sesarma_cpue",
+         variables != "uca_cpue") %>%
+  mutate(habitat = "bcb")%>%
+  gather(crab_param, pearson_cor,2:5) %>%
+  select(habitat,crab_params, env_params = variables, pearson_cor)
+
 bcb_corr_n <- data.frame(bcb_corr$n) %>%
   mutate(variables = row.names(.)) %>%
   select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
@@ -22,10 +70,7 @@ bcb_corr_p <- data.frame(bcb_corr$p) %>%
   select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
   
   
-  data.frame(.$r) %>%
-  mutate(variables = row.names(.)) %>%
-  select(variables,burrow_density,carcinus_cpue,sesarma_cpue,uca_cpue)
-bcb_corr
+ 
 
   
 
